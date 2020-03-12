dca68836387191e12a1f93eb6fec152e1427d67d
nysa <nysa.sunny@gmail.com>
2020-03-12 19:13:34 +0100

    done for the day

--- day-n-night/index.js
+++ day-n-night/index.js
@@ -1,10 +1,10 @@
 document.querySelector('.toggle').addEventListener('change', e => {
   if(e.target.checked) {
-    document.querySelectorAll('body, .article-container, .sidebar, .line, .panel-line').forEach(e => {
+    document.querySelectorAll('body, .navbar-container, .article-container, .sidebar, .line, .panel-line').forEach(e => {
       e.classList.add('darkness')
     })
   } else {
-    document.querySelectorAll('body, .article-container, .sidebar, .line, .panel-line').forEach(e => {
+    document.querySelectorAll('body, .navbar-container, .article-container, .sidebar, .line, .panel-line').forEach(e => {
       e.classList.remove('darkness')
     })
   }

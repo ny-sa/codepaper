976eb16c92f8deb93fed38e4b15ea95310b33aee
nysa <nysa.sunny@gmail.com>
2020-03-14 18:26:20 +0100

    tie up dark mode toggle

--- day-n-night/index.js
+++ day-n-night/index.js
@@ -1,11 +1,23 @@
-document.querySelector('.toggle').addEventListener('change', e => {
-  if(e.target.checked) {
-    document.querySelectorAll('body, .navbar-container, .article-container, .sidebar, .line, .panel-line').forEach(e => {
-      e.classList.add('darkness')
-    })
-  } else {
-    document.querySelectorAll('body, .navbar-container, .article-container, .sidebar, .line, .panel-line').forEach(e => {
-      e.classList.remove('darkness')
-    })
+//Check OS preference, but only once
+if (window.matchMedia('(prefers-color-scheme: dark)').matches && !window.localStorage.getItem('didItAlready')) {
+  window.localStorage.setItem('isDarkMode', 'true');
+  window.localStorage.setItem('didItAlready', '!!!');
+}
+
+//Toggle
+document.querySelector('.toggle').addEventListener('change', () => {
+  window.localStorage.setItem('isDarkMode', (window.localStorage.getItem('isDarkMode') === 'false') ? 'true' : 'false');
+  document.querySelectorAll('body, .circle, .navbar-container, .article-container, .sidebar, .line, .bar-line').forEach(e => {
+    e.classList.toggle('darkness')
+  });
+});
+
+//Check browser on next loading
+window.onload = () => {
+  if (window.localStorage.getItem('isDarkMode') === 'true') {
+    document.querySelectorAll('body, .circle, .navbar-container, .article-container, .sidebar, .line, .bar-line').forEach(e => {
+      e.classList.toggle('darkness');
+    });
+    document.querySelector('.toggle').checked = true;
   }
-})
+};
